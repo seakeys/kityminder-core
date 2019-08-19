0f53ca732fbd536e6e197360e934002414b04448
chenhaifei <649719596@qq.com>
2019-08-19 18:13:34 +0800

    恢复

--- src/core/template.js
+++ src/core/template.js
@@ -1,7 +1,10 @@
 define(function(require, exports, module) {
     var kity = require('./kity');
+    var utils = require('./utils');
     var Minder = require('./minder');
+    var Command = require('./command');
     var MinderNode = require('./node');
+    var Module = require('./module');
 
     var _templates = {};
 
@@ -10,6 +13,12 @@ define(function(require, exports, module) {
     }
     exports.register = register;
 
+    utils.extend(Minder, {
+        getTemplateList: function() {
+            return _templates;
+        }
+    });
+
     kity.extendClass(Minder, (function() {
         var originGetTheme = Minder.prototype.getTheme;
         return {
@@ -17,8 +26,17 @@ define(function(require, exports, module) {
                 this.setTemplate(name);
                 this.refresh(duration || 800);
             },
+
+            getTemplate: function() {
+                return this._template || 'default';
+            },
+
+            setTemplate: function(name) {
+                this._template = name || null;
+            },
+
             getTemplateSupport: function(method) {
-                var supports = _templates['right'];
+                var supports = _templates[this.getTemplate()];
                 return supports && supports[method];
             },
 
@@ -45,4 +63,30 @@ define(function(require, exports, module) {
             }
         };
     })());
+
+    Module.register('TemplateModule', {
+        /**
+         * @command Template
+         * @description 设置当前脑图的模板
+         * @param {string} name 模板名称
+         *    允许使用的模板可以使用 `kityminder.Minder.getTemplateList()` 查询
+         * @state
+         *   0: 始终可用
+         * @return 返回当前的模板名称
+         */
+        commands: {
+            'template': kity.createClass('TemplateCommand', {
+                base: Command,
+
+                execute: function(minder, name) {
+                    minder.useTemplate(name);
+                    minder.execCommand('camera');
+                },
+
+                queryValue: function(minder) {
+                    return minder.getTemplate() || 'default';
+                }
+            })
+        }
+    });
 });
